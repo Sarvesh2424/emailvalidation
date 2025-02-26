473aa14bfdee824c2ccb96a1821dab4e095fa1c3
Sarvesh2424 <10650sarvesh@gmail.com>
2025-02-26 22:08:49 +0530

    2nd

--- src/Home.jsx
+++ src/Home.jsx
@@ -28,9 +28,9 @@ export default function Home({ setEmailsList }) {
               <span className="block">Validate Your Emails</span>
               <span className="block text-yellow-400">with Ease</span>
             </h1>
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl ">
-              Organize your email lists and protect your
-              sender reputation with this powerful email validation tool.
+            <p className="mt-3 max-w-md mx-auto text-base text-gray-400 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl ">
+              Organize your email lists and increase your
+              productivity with this powerful email validation tool.
             </p>
           </div>
         </div>
